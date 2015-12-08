28835773f8729d294c0a64749a336ffe39f816d0
Lauren Ogawa <logawa12@yahoo.com>
2015-12-08 08:01:55 -1000

    More Fish

--- app/src/main/java/edu/hawaii/test1/MainActivity.java
+++ app/src/main/java/edu/hawaii/test1/MainActivity.java
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
@@ -39,48 +40,62 @@ public class MainActivity extends Activity
                 c.moveToFirst();
                 int unreadMessagesCount = c.getCount();
 
+                ImageView myImageView = (ImageView) findViewById(R.id.fish1);
+                ImageView myImageView_2 = (ImageView) findViewById(R.id.fish2);
+                ImageView myImageView_3 = (ImageView) findViewById(R.id.fish3);
+
+                TextView myTextView = (TextView) findViewById(R.id.textView);
+
+                myTextView.setText("Message count: " + unreadMessagesCount);
+
+                AnimatorSet fishSet = (AnimatorSet) AnimatorInflater.loadAnimator(MainActivity.this, R.animator.animation);;
+                AnimatorSet fishSet_2 = (AnimatorSet) AnimatorInflater.loadAnimator(MainActivity.this, R.animator.animation);;
+                AnimatorSet fishSet_3 = (AnimatorSet) AnimatorInflater.loadAnimator(MainActivity.this, R.animator.animation);;
+
+                fishSet.setTarget(myImageView);
+                fishSet_2.setTarget(myImageView_2);
+                fishSet_3.setTarget(myImageView_3);
+
+                Bitmap bit;
+                Bitmap bit_2;
+                Bitmap bit_3;
+
                 if (unreadMessagesCount > 0) {
-
-                    ImageView myImageView = (ImageView) findViewById(R.id.fish1);
-                    ImageView myImageView_2 = (ImageView) findViewById(R.id.fish2);
-                    ImageView myImageView_3 = (ImageView) findViewById(R.id.fish3);
-
-                    Bitmap bit;
-                    Bitmap bit_2;
-                    Bitmap bit_3;
 
                     if (unreadMessagesCount == 1) {
                         bit = BitmapFactory.decodeResource(getResources(), R.drawable.fish);
                         myImageView.setImageBitmap(bit);
+
+                        fishSet.start();
+
                     }else if (unreadMessagesCount == 2) {
                         bit = BitmapFactory.decodeResource(getResources(), R.drawable.fish);
                         bit_2 = BitmapFactory.decodeResource(getResources(), R.drawable.fish_2);
+
                         myImageView.setImageBitmap(bit);
                         myImageView_2.setImageBitmap(bit_2);
+
+                        fishSet.start();
+                        fishSet_2.start();
+
                     }else {
                         bit = BitmapFactory.decodeResource(getResources(), R.drawable.fish);
                         bit_2 = BitmapFactory.decodeResource(getResources(), R.drawable.fish_2);
                         bit_3 = BitmapFactory.decodeResource(getResources(), R.drawable.fish_3);
+
                         myImageView.setImageBitmap(bit);
                         myImageView_2.setImageBitmap(bit_2);
                         myImageView_3.setImageBitmap(bit_3);
+
+                        fishSet.start();
+                        fishSet_2.start();
+                        fishSet_3.start();
+
                     }
-
-                    AnimatorSet fishSet = (AnimatorSet) AnimatorInflater.loadAnimator(MainActivity.this, R.animator.animation);
-                    AnimatorSet fishSet_2 = (AnimatorSet) AnimatorInflater.loadAnimator(MainActivity.this, R.animator.animation);
-                    AnimatorSet fishSet_3 = (AnimatorSet) AnimatorInflater.loadAnimator(MainActivity.this, R.animator.animation);
-
-                    fishSet.setTarget(myImageView);
-                    fishSet_2.setTarget(myImageView_2);
-                    fishSet_3.setTarget(myImageView_3);
-
-                    fishSet.start();
-                    fishSet_2.start();
-                    fishSet_3.start();
-
                 }
 
                 handler.postDelayed(this, time);
+
             }
         }, time);
     }
